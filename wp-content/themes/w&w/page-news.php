da60c6e97979ebe881a176383d1dbbd8683a2098
Duncan Thomson <duncan@elevate.com>
2013-12-31 17:16:44 -0700

    News Page
    News loop and framework created
    
    Signed-off-by: Duncan Thomson <duncan@elevate.com>

--- wp-content/themes/w&w/page-news.php
+++ wp-content/themes/w&w/page-news.php
@@ -3,5 +3,29 @@
 ?>
 <?php get_header(); ?>
     <section class="clearfix">
+        <div class="news-container">
+            <?php
+            $news=array(
+                'post_type' => 'news',
+                'posts_per_page' => 3000
+            );
+            // The Query
+            query_posts($news);
+            // The Loop
+            if (have_posts()) {
+                while (have_posts()){
+                    the_post();
+                    echo"<ul class='news'><li class='news-photo'>";
+                    the_post_thumbnail();
+                    echo"</li><li class='news-content'>";
+                    the_content();
+                    echo"</li></ul>";
+                }// end while
+            }// end if
+            wp_reset_query();
+            // Reset Query
+            wp_reset_postdata();
+            ?>
+        </div>
     </section>
 <?php get_footer(); ?>
