e6f7f0a971df0cf186eb005226cfc7d75abde339
Duncan Thomson <duncan@elevate.com>
2014-01-02 15:38:43 -0700

    Contact
    Contact us cascade order change.
    
    Signed-off-by: Duncan Thomson <duncan@elevate.com>

--- wp-content/themes/w&w/page-contact.php
+++ wp-content/themes/w&w/page-contact.php
@@ -3,6 +3,23 @@
 ?>
 <?php get_header(); ?>
     <section class="clearfix">
+        <div class="contact-right">
+            <?php
+                // The Query
+                if (have_posts()) {
+                    // The Loop
+                    while (have_posts()){
+                        the_post();
+                        echo"<div>";
+                        the_field('contact_map');
+                        echo"</div>";
+                    }// end while
+                }// end if
+                wp_reset_query();
+                // Reset Query
+                wp_reset_postdata();
+            ?>
+        </div>
         <div class="contact-left">
             <?php
             // The Query
@@ -16,23 +33,6 @@
             // Reset Query
             wp_reset_postdata();
             ?>
-            </div>
-            <div class="contact-right">
-                <?php
-                    // The Query
-                    if (have_posts()) {
-                        // The Loop
-                        while (have_posts()){
-                            the_post();
-                            echo"<div>";
-                            the_field('contact_map');
-                            echo"</div>";
-                        }// end while
-                    }// end if
-                    wp_reset_query();
-                    // Reset Query
-                    wp_reset_postdata();
-                ?>
-            </div>
+        </div>
         </section>
 <?php get_footer(); ?>
